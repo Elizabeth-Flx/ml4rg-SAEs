84289b011517e90c79748c729e5a84a5b18a0dfb
Elizabeth Lochert <daniel.lochert.flx@gmail.com>
2025-06-27 11:27:30 +0200

    first test run

--- src/models/simple_sae.py
+++ src/models/simple_sae.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 
 class SAE(nn.Module):
-    def __init__(self, input_dim, latent_space_dim, encoder_dims=[100, 50], decoder_dims=None):
-        super(SAE, self).__init__()
-
+    def __init__(self, input_dim, latent_space_dim, encoder_dims=[1000], decoder_dims=None):
+        super().__init__()
+        
         encoder_layers = [nn.Linear(input_dim, encoder_dims[0]), nn.ReLU()]
         for i in range(len(encoder_dims) - 1):
             encoder_layers.append(nn.Linear(encoder_dims[i], encoder_dims[i + 1]))
@@ -22,11 +22,11 @@ class SAE(nn.Module):
             decoder_layers.append(nn.Linear(decoder_dims[i], decoder_dims[i+1]))
             decoder_layers.append(nn.ReLU())
         decoder_layers.append(nn.Linear(decoder_dims[-1], input_dim))
-        decoder_layers.append.append(nn.Sigmoid())          # adjust this based on the embedding range
+        # decoder_layers.append(nn.Sigmoid())          # adjust this based on the embedding range
 
         self.decoder = nn.Sequential(*decoder_layers)
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        latent = self.encoder(x)
+        recon = self.decoder(latent)
+        return recon, latent
